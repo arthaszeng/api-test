0cdc57b68418685241f51bf778e7d4eb1ce81330
mengyang <mengyang.sun@thoughtworks.com>
2020-08-21 11:27:33 +0800

    chore: delete sonar

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,26 +18,26 @@ pipeline {
                     }
                 }
 
-                stage("Sonar Analysis") {
-                    environment {
-                        scannerHome = tool 'SonarQubeScanner'
-                    }
-                    steps {
-                        script {
-                            withSonarQubeEnv('sonarqube') {
-                                sh "${scannerHome}/bin/sonar-scanner -Dproject.settings=sonar-project.properties"
-                            }
-
-                            sleep(10)
-
-                            qualitygate = waitForQualityGate()
-                            if (qualitygate.status != "OK") {
-                                currentBuild.result = "FAILURE"
-                                slackSend(channel: '#loyalty-program-cicd', color: '#F01717', message: "*$JOB_NAME*, <$BUILD_URL|Build #$BUILD_NUMBER>: Code coverage threshold was not met! <http://****.com:9000/sonarqube/projects|Review in SonarQube>.")
-                            }
-                        }
-                    }
-                }
+//                stage("Sonar Analysis") {
+//                    environment {
+//                        scannerHome = tool 'SonarQubeScanner'
+//                    }
+//                    steps {
+//                        script {
+//                            withSonarQubeEnv('sonarqube') {
+//                                sh "${scannerHome}/bin/sonar-scanner -Dproject.settings=sonar-project.properties"
+//                            }
+//
+//                            sleep(10)
+//
+//                            qualitygate = waitForQualityGate()
+//                            if (qualitygate.status != "OK") {
+//                                currentBuild.result = "FAILURE"
+//                                slackSend(channel: '#loyalty-program-cicd', color: '#F01717', message: "*$JOB_NAME*, <$BUILD_URL|Build #$BUILD_NUMBER>: Code coverage threshold was not met! <http://****.com:9000/sonarqube/projects|Review in SonarQube>.")
+//                            }
+//                        }
+//                    }
+//                }
             }
         }
     }
